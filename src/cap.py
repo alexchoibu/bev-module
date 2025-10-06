dee47e8c2efae612d1d08e0e567af5c0d4ba0342
alexchoibu <alexchoi@bu.edu>
2025-10-06 18:02:04 -0400

    Updated capture code to identify connected cameras dynamically

--- src/cap.py
+++ src/cap.py
@@ -1,11 +1,17 @@
 import cv2
+from cv2_enumerate_cameras import enumerate_cameras
 
-for i in range(3):
-    cap = cv2.VideoCapture(i) # Check the correct camera index
+for camera_info in enumerate_cameras(cv2.CAP_AVFOUNDATION):
+    print("Found camera:", camera_info.name, "at index", camera_info.index)
+
+    cap = cv2.VideoCapture(camera_info.index) # Check the correct camera index
 
     # Set resolution to 12MP, assuming 4000x3000
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4000)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 3000)
+
+    # Set auto exposure
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 40)
 
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
@@ -14,11 +20,11 @@ for i in range(3):
         ret, frame = cap.read()
         if not ret:
             print("Empty frame received")
-        else:
-            print("Frame " + str(i) + " successfully read")
+        elif i >= 5:
+            print("Camera", camera_info.index, "frame successfully read")
             cv2.imshow('Input', frame)
             cv2.waitKey(0)
-            cv2.imwrite("frame_" + str(i) + ".png", frame)
+            #cv2.imwrite("frame_" + str(i) + ".png", frame)
             cv2.destroyAllWindows()
             break
 
